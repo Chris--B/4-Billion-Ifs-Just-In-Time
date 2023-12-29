34eff505e4f9b8a58fa830f697ca0d58a64f65f8
Chris Butler <chrisbutler296@gmail.com>
2023-12-28 21:10:29 -0700

    She works!
    Except for the numbers where she doesn't. 0x75 stores the relative jump
    as a signed offset, so it fails at 128 not 256.

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,84 @@ use std::ops::{Deref, DerefMut};
 
 const KiB: usize = 1 << 10;
 
-fn main() {
-    let mut jit = JitMem::new();
+#[track_caller]
+#[must_use]
+fn write(to: &mut [u8], from: impl AsRef<[u8]>) -> &mut [u8] {
+    let from = from.as_ref();
+    let n = from.len();
+    to[..n].copy_from_slice(from);
 
-    assert_eq!(std::env::consts::ARCH, "x86_64");
-    let mut return_int = [
-        0x48, 0xC7, 0xC0, 0x01, 0x00, 0x00, 0x00, // mov rax, 0x1
-        0xC3, // ret
+    &mut to[n..]
+}
+
+fn build_is_odd(jit: &mut JitMem) -> fn(i64) -> i64 {
+    let header: [u8; 7] = [
+        // move rax, 0x0
+        0x48, 0xc7, 0xc0, 0x00, 0x00, 0x00, 0x00,
     ];
 
-    unsafe {
-        return_int[3] = 7;
-        jit[..return_int.len()].copy_from_slice(&return_int);
+    #[rustfmt::skip]
+    let mut block : [u8; 14]= [
+        // cmp rdi, 0x0
+        //          ^^^---vvvv
+        0x48, 0x83, 0xff, 0x00,
+        // Jump relative 0x08 ahead (this matches the end of this array)
+        // See: https://faydoc.tripod.com/cpu/jne.htm
+        // jne 0x08
+        0x75, 0x08,
+        // mov rax, 0x1
+        //          ^^^---vvvv
+        0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00,
+        // ret
+        0xc3,
+    ];
 
-        let seven = jit.make_fn();
-        dbg!(seven());
+    let mut mem = jit.as_slice_mut();
+
+    #[allow(unused_assignments)]
+    unsafe {
+        mem = write(mem, header);
+
+        for n in 0..=u8::MAX {
+            block[3] = n;
+            block[9] = n & 1;
+            mem = write(mem, block);
+        }
+
+        jit.make_fn()
+    }
+}
+
+fn main() {
+    assert_eq!(std::env::consts::ARCH, "x86_64");
+
+    let mut jit = JitMem::new();
+    let is_odd = build_is_odd(&mut jit);
+
+    for n in 0..(u8::MAX as i64) {
+        let is_n_odd = is_odd(n);
+        println!("is_odd({n}) == {}", is_n_odd);
+    }
+}
+
+#[test]
+fn check_u8_nums() {
+    let mut jit = JitMem::new();
+    let is_odd = build_is_odd(&mut jit);
+
+    for n in 0..(u8::MAX as i64) {
+        let is_n_odd = is_odd(n);
+        if n & 1 == 0 {
+            assert!(
+                is_n_odd == 0,
+                "is_odd({n}) reports that {n} is odd but it's even!"
+            );
+        } else {
+            assert!(
+                is_n_odd == 1,
+                "is_odd({n}) reports that {n} is even but it's odd!"
+            );
+        }
     }
 }
 
@@ -50,8 +113,8 @@ impl JitMem {
             let _ = posix_memalign(&mut p_mem, PAGE_SIZE, size);
             mprotect(p_mem, size, PROT_EXEC | PROT_READ | PROT_WRITE);
 
-            // x64 'RET'
-            // Good luck, Rosetta
+            // x64 'RET', anything that lands in "uninit" memory here will immediately return
+            // We could also fault....
             memset(p_mem, 0xC3, size);
 
             Self {
@@ -69,7 +132,7 @@ impl JitMem {
         unsafe { core::slice::from_raw_parts_mut(self.p_mem, self.size) }
     }
 
-    unsafe fn make_fn(&self) -> fn() -> i64 {
+    unsafe fn make_fn(&self) -> fn(i64) -> i64 {
         core::mem::transmute(self.p_mem)
     }
 }
@@ -90,8 +153,9 @@ impl DerefMut for JitMem {
 
 impl Drop for JitMem {
     fn drop(&mut self) {
-        unsafe {
-            free(self.p_mem as *mut c_void);
-        }
+        // We can't track function pointer lifetimes, so just... don't
+        // unsafe {
+        //     free(self.p_mem as *mut c_void);
+        // }
     }
 }
