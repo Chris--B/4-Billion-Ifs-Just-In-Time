387143363e02a38ec4e3af5c0cb2316d14985c6a
Chris Butler <chrisbutler296@gmail.com>
2023-12-28 21:39:35 -0700

    Misc Tweaks
    Attempting to expand to 32-bit support but it keeps crashing.
    Saving these changes in a known-good state.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![allow(non_upper_case_globals)]
+#![allow(unused, non_upper_case_globals, unused_assignments)]
 
 use libc::*;
 
@@ -6,8 +6,10 @@ use std::ops::{Deref, DerefMut};
 
 const KiB: usize = 1 << 10;
 
-#[track_caller]
+const MAX_SUPPORTED_NUM: u32 = 200;
+
 #[must_use]
+#[track_caller]
 fn write(to: &mut [u8], from: impl AsRef<[u8]>) -> &mut [u8] {
     let from = from.as_ref();
     let n = from.len();
@@ -16,42 +18,58 @@ fn write(to: &mut [u8], from: impl AsRef<[u8]>) -> &mut [u8] {
     &mut to[n..]
 }
 
+#[cfg(target_arch = "x86_64")]
 fn build_is_odd(jit: &mut JitMem) -> fn(i64) -> i64 {
     let header: [u8; 7] = [
         // move rax, 0x0
         0x48, 0xc7, 0xc0, 0x00, 0x00, 0x00, 0x00,
     ];
 
+    // Useful references:
+    //      https://defuse.ca/online-x86-assembler.htm
+    //      https://shell-storm.org/online/Online-Assembler-and-Disassembler/
+    //      https://faydoc.tripod.com/cpu/jne.htm
     #[rustfmt::skip]
     let mut block : [u8; 14]= [
         // cmp rdi, 0x0
         //          ^^^---vvvv
         0x48, 0x83, 0xff, 0x00,
-        // Jump relative 0x08 ahead (this matches the end of this array)
-        // See: https://faydoc.tripod.com/cpu/jne.htm
+
+        // Jump relative 8 ahead (this matches the end of this array)
         // jne 0x08
         0x75, 0x08,
+
         // mov rax, 0x1
         //          ^^^---vvvv
         0x48, 0xc7, 0xc0, 0x01, 0x00, 0x00, 0x00,
+
         // ret
         0xc3,
     ];
 
-    let mut mem = jit.as_slice_mut();
+    for b in block {
+        print!("{b:02x}");
+    }
+    println!();
 
-    #[allow(unused_assignments)]
+    let mut mem = jit.as_slice_mut();
     unsafe {
         mem = write(mem, header);
 
-        for n in 0..=u8::MAX {
-            block[3] = n;
-            block[9] = n & 1;
+        for n in 0..=MAX_SUPPORTED_NUM {
+            block[3] = n as u8;
+            block[9] = (n & 1) as u8;
+
             mem = write(mem, block);
         }
 
         jit.make_fn()
     }
+}
+
+#[cfg(target_arch = "aarch64")]
+fn build_is_odd(_jit: &mut JitMem) -> fn(i64) -> i64 {
+    unimplemented!()
 }
 
 fn main() {
@@ -60,8 +78,8 @@ fn main() {
     let mut jit = JitMem::new();
     let is_odd = build_is_odd(&mut jit);
 
-    for n in 0..(u8::MAX as i64) {
-        let is_n_odd = is_odd(n);
+    for n in 0..MAX_SUPPORTED_NUM {
+        let is_n_odd = is_odd(n as i64);
         println!("is_odd({n}) == {}", is_n_odd);
     }
 }
@@ -71,18 +89,12 @@ fn check_u8_nums() {
     let mut jit = JitMem::new();
     let is_odd = build_is_odd(&mut jit);
 
-    for n in 0..(u8::MAX as i64) {
-        let is_n_odd = is_odd(n);
+    for n in 0..MAX_SUPPORTED_NUM {
+        let is_n_odd = is_odd(n as i64);
         if n & 1 == 0 {
-            assert!(
-                is_n_odd == 0,
-                "is_odd({n}) reports that {n} is odd but it's even!"
-            );
+            assert!(is_n_odd == 0, "is_odd({n}) == {is_n_odd}, but {n} is even!");
         } else {
-            assert!(
-                is_n_odd == 1,
-                "is_odd({n}) reports that {n} is even but it's odd!"
-            );
+            assert!(is_n_odd == 1, "is_odd({n}) == {is_n_odd}, but {n} is odd!");
         }
     }
 }
@@ -111,6 +123,8 @@ impl JitMem {
             // MacOS has alignment requirements on executabe pages
 
             let _ = posix_memalign(&mut p_mem, PAGE_SIZE, size);
+            println!("JIT memory at 0x{:0x}", p_mem as usize);
+
             mprotect(p_mem, size, PROT_EXEC | PROT_READ | PROT_WRITE);
 
             // x64 'RET', anything that lands in "uninit" memory here will immediately return
